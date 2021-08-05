329cd8ad01301efe94986411d5c04a83d6eda5f8
Andraea Zambakides <azambakides@gmail.com>
2021-08-05 11:40:47 +1000

    Adding first files of project set-up

---
+++ src/main/java/com/andraea/app/App.java
@@ -0,0 +1,13 @@
+package com.andraea.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/andraea/app/AppTest.java
@@ -0,0 +1,13 @@
+package com.andraea.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
