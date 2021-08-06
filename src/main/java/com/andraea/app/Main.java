ea109b110d34647281c22842de4afdbede37851c
Andraea Zambakides <azambakides@gmail.com>
2021-08-06 14:44:35 +1000

    Printed out a welcome message to the console with all four shows at different dates and times.

--- src/main/java/com/andraea/app/Main.java
+++ src/main/java/com/andraea/app/Main.java
@@ -7,9 +7,13 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to The Zamba Theatre" );
+        System.out.println( "Welcome to The Zamba Theatre!" );
         System.out.println( "Which of these shows would you like to see?");
+
         System.out.println(Show.TheNutcracker.getShowName() + " " + Show.TheNutcracker.getShowDate() + " " + Show.TheNutcracker.getShowTime1());
+        System.out.println(Show.AnInspectorCalls.getShowName() + " " + Show.AnInspectorCalls.getShowDate() + " " + Show.AnInspectorCalls.getShowTime2());
+        System.out.println(Show.JosephAndTheAmazingTechnicolorDreamcoat.getShowName() + " " + Show.JosephAndTheAmazingTechnicolorDreamcoat.getShowDate() + " " + Show.JosephAndTheAmazingTechnicolorDreamcoat.getGetShowTime3());
+        System.out.println(Show.LaBoheme.getShowName() + " " + Show.LaBoheme.getShowDate() + " " + Show.LaBoheme.getShowTime1());
 
 //        ShowTemplates showTemplatesOne = new ShowTemplates();
 //        showTemplatesOne.setNameOfTheShow("The Nutcracker");
