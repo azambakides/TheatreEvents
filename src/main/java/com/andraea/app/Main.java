9383ea372a5a5e8ded9d2ee073fe0f7528a6f2c7
Andraea Zambakides <azambakides@gmail.com>
2021-08-09 18:18:07 +1000

    Created the User Input: deleted original second greeting and made a Conditional If/Else Statement with the console of "if" printing the correct input show with the name and a parting statement, and "else" apologising for the show not being found

--- src/main/java/com/andraea/app/Main.java
+++ src/main/java/com/andraea/app/Main.java
@@ -9,17 +9,23 @@ public class Main {
 
     public static void main( String[] args ) {
         System.out.println("Welcome to The Zamba Theatre!");
-        System.out.println("What show would you like to look for?");
 
-//        Scanner select = new Scanner(System.in);
-//        Scanner choice = new Scanner(System.in);
-//
-//        System.out.println("Please enter the name of the show:");
+        Scanner selectShow = new Scanner(System.in);
+        System.out.println("Please enter the name of the show you want to see: ");
 
-        for (TheatreTemplates theatreTemplates : Show.getAllShows()) {
-            theatreTemplates.printShow();
+        String inputByUser = selectShow.nextLine();
+
+        boolean foundSelectedShow = false;
+        for (TheatreTemplates showIteration : Show.getAllShows()) {
+            if(inputByUser.equalsIgnoreCase(showIteration.getShowName())) {
+                showIteration.printShow();
+                foundSelectedShow = true;
+            }
         }
-
+        if(!foundSelectedShow){
+            System.out.println("Oops! Sorry. This show cannot be found.");
+        } else {
+            System.out.println("Enjoy the show!");
+        }
     }
-
 }
